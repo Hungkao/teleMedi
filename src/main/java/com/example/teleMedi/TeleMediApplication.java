e66cb1929a330a0f5ac003bf741ebad27e98585a
PhucHung <nph04022004@gmail.com>
2026-02-25 07:27:37 +0700

    Project created

---
+++ src/main/java/com/example/teleMedi/TeleMediApplication.java
@@ -0,0 +1,13 @@
+package com.example.teleMedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleMediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleMediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teleMedi/TeleMediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teleMedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleMediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
